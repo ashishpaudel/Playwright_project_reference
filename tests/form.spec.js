d51e236d9998b994a379cb0a2246807b479d7863
Aashish Poudel <aashish.poudel@ime.com.np>
2025-07-22 17:48:40 +0545

    updated on test cases

--- tests/form.spec.js
+++ tests/form.spec.js
@@ -2,6 +2,7 @@ import { expect, test } from "@playwright/test";
 import { form } from "../POM/page";
 import {formVariable}from "../POM/variable"
 import { stateCityMapping } from "../POM/variable";
+import { expectedData } from "../POM/variable";
 import { KeyObject } from "crypto";
 
     test.beforeEach(async({page})=>{
@@ -9,47 +10,49 @@ import { KeyObject } from "crypto";
 })
 test.describe('Form test', () => {
     
-    test.skip('should submit successfully with all valid data', async ({ page },testData) => {
+    test('should submit successfully with all valid data', async ({ page },testData) => {
         const Form = new form(page)
+        const fullName = expectedData["Student Name"].split(" ")
         await Form.enterPersonalInfo(
-            'Ashish',
-            'Paudel',
-            'ashish@gmail.com',
-            '9823875819')
-        await Form.genderSlection('male')
+            fullName[0],
+            fullName[1],
+            expectedData["Student Email"],
+            expectedData["Mobile"]
+        )
+            
+        await Form.genderSlection(expectedData["Gender"].toLowerCase());
+        // await Form.genderSlection(expectedData.Gender)
         await Form.selectDateOfBirth('1998','7')
-        await page.waitForTimeout(100)
+        // await page.waitForTimeout(100)
 
-        await Form.selectSubject('computer science')
-        await Form.hobbies('Music')
-        await page.waitForTimeout(100)
-        await Form.addImage('image/camera.png')
-        await Form.currentAdd('kathmandu')
-        await page.waitForTimeout(1000)
-        await Form.slectState('Uttar Pradesh')        
-        await Form.slectCity('agra')
+        await Form.selectSubject(expectedData["Subjects"])
+        await Form.hobbies(expectedData["Hobbies"])
+        // await page.waitForTimeout(100)
+        await Form.addImage(`image/${expectedData["Picture"]}`)
+        await Form.currentAdd(expectedData["Address"])
+        // await page.waitForTimeout(1000)
+        await Form.selectState(1)        
+        await Form.selectCity(0)
         await Form.submit()
-        await page.waitForTimeout(1000)
-        await expect(page.locator('#example-modal-sizes-title-lg')).toHaveText('Thanks for submitting the form');
+        // await page.waitForTimeout(1000)
 
-        await expect(page.locator('//td[normalize-space()="Student Name"]/following-sibling::td')).toHaveText('Ashish Paudel')
-        await expect(page.locator('//td[normalize-space()="Student Email"]/following-sibling::td')).toHaveText('ashish@gmail.com')
-        await expect(page.locator('//td[normalize-space()="Gender"]/following-sibling::td')).toHaveText('Male')
-        await expect(page.locator('//td[normalize-space()="Mobile"]/following-sibling::td')).toHaveText('9823875819')
-        await expect(page.locator('//td[normalize-space()="Date of Birth"]/following-sibling::td')).toHaveText('15 August,1998')
-        await expect(page.locator('//td[normalize-space()="Subjects"]/following-sibling::td')).toHaveText('Computer Science')
-        await expect(page.locator('//td[normalize-space()="Hobbies"]/following-sibling::td')).toHaveText('Music')
-        await expect(page.locator('//td[normalize-space()="Picture"]/following-sibling::td')).toHaveText('camera.png')
-        await expect(page.locator('//td[normalize-space()="Address"]/following-sibling::td')).toHaveText('kathmandu')
-        await expect(page.locator('//td[normalize-space()="State and City"]/following-sibling::td')).toHaveText('Uttar Pradesh Agra')
+        await expect(page.locator('#example-modal-sizes-title-lg')).toHaveText('Thanks for submitting the form');
         
+        await expect(page.locator('#example-modal-sizes-title-lg')).toHaveText("Thanks for submitting the form")
+        
+        for(const [key,value] of Object.entries(expectedData)){
+
+            const locator = page.locator(`//td[normalize-space()="${key}"]/following-sibling::td`)
+            await expect(locator).toContainText(value)
+        }
+
         await Form.closeBtn()
         await expect(page.locator('.text-center')).toHaveText('Practice Form')
         await page.waitForTimeout(1000)
     })
 
-    test.skip('should show validation errors when submitting empty form',async({page})=>{
-        // const Form = new form(page)
+    test('should show validation errors when submitting empty form',async({page})=>{
+        const Form = new form(page)
         await Form.submit()
         await expect(page.locator('#firstName')).toHaveCSS('border-color', 'rgb(220, 53, 69)')
         await expect(page.locator('#lastName')).toHaveCSS('border-color', 'rgb(220, 53, 69)')
@@ -57,32 +60,34 @@ test.describe('Form test', () => {
         await expect(page.locator('//label[normalize-space()="Female"]')).toHaveCSS('border-color','rgb(220, 53, 69)')
     })
 
-    test.skip('should submit successfully with only required fields',async({page})=>{
+    test('should submit successfully with only required fields',async({page})=>{
                 const Form = new form(page)
+        const fullName = expectedData["Student Name"].split(" ")
         await Form.enterPersonalInfo(
-            'Ashish',
-            'Paudel',
-            'ashish@gmail.com',
-            '9823875819')
-        await page.waitForTimeout(100)
-        await Form.genderSlection('male')
+            fullName[0],
+            fullName[1],
+            expectedData["Student Email"],
+            expectedData["Mobile"]
+        )
+            
+        await Form.genderSlection(expectedData["Gender"].toLowerCase());
         await Form.submit()
         await expect(page.locator('#example-modal-sizes-title-lg')).toHaveText('Thanks for submitting the form')
     })
 
-    test.skip('should validate correct email format',async({page})=>{
+    test('should validate correct email format',async({page})=>{
                 const Form = new form(page)
         await Form.enterPersonalInfo(
             '',
             '',
-            'ashish@gmail.com',
+             expectedData["Student Email"],
             '')
         await Form.submit()
         
         await expect(page.locator('#userEmail')).toHaveCSS('border-color', 'rgb(40, 167, 69)')
     })
 
-    test.skip('should show error for invalid email format',async({page})=>{
+    test('should show error for invalid email format',async({page})=>{
         const Form = new form(page)
         await Form.enterPersonalInfo(
             '',
@@ -94,7 +99,7 @@ test.describe('Form test', () => {
         await expect(page.locator('#userEmail')).toHaveCSS('border-color',  'rgb(220, 53, 69)')
     })
 
-    test.skip('should correctly toggle gender selection',async({page})=>{
+    test('should correctly toggle gender selection',async({page})=>{
         const Form = new form(page)
         await Form.genderSlection('male')
         
@@ -109,7 +114,7 @@ test.describe('Form test', () => {
         await expect(page.locator('//label[normalize-space()="Other"]')).toBeChecked()
     })
 
-    test.skip('should show error for short mobile number',async({page})=>{
+    test('should show error for short mobile number',async({page})=>{
         const Form = new form(page)
         await Form.enterPersonalInfo(
             '',
@@ -121,7 +126,7 @@ test.describe('Form test', () => {
         await expect(page.locator('#userNumber')).toHaveCSS('border-color',  'rgb(220, 53, 69)')
     })
 
-    test.skip('should show error for non-numeric mobile number',async({page})=>{
+    test('should show error for non-numeric mobile number',async({page})=>{
         const Form = new form(page)
         await Form.enterPersonalInfo(
             '',
@@ -130,18 +135,18 @@ test.describe('Form test', () => {
             'abcdefghij')
         await Form.submit()
         
-        await expect(page.locator('#')).toHaveCSS('border-color',  'rgb(220, 53, 69)')
+        await expect(page.locator('#userNumber')).toHaveCSS('border-color',  'rgb(220, 53, 69)')
     })
 
-    test.skip('should handle invalid subject input',async({page})=>{
+    test('should handle invalid subject input',async({page})=>{
         const Form = new form(page)
         await Form.selectSubject('asdasdf')
         await Form.submit()
         
-    await expect(page.locator('.subjects-auto-complete__multi-value__label')).toHaveCount(0)       
+    await expect(page.locator('.subjects-auto-complete__multi-value__label')).toBeVisible()     
     })
 
-    test.skip('should accept valid subjects',async({page})=>{
+    test('should accept valid subjects',async({page})=>{
         const Form = new form(page)
         await Form.selectSubject('computer science')
         await Form.selectSubject('physics')
@@ -150,7 +155,7 @@ test.describe('Form test', () => {
     await expect(page.locator('.subjects-auto-complete__multi-value__label', { hasText: ('Computer Science','Physics') })).toBeVisible()        
     })
 
-    test.skip('should remove subjects using close button',async({page})=>{
+    test('should remove subjects using close button',async({page})=>{
         const Form = new form(page)
         await Form.selectSubject('computer science')
         await Form.selectSubject('physics')
@@ -161,7 +166,7 @@ test.describe('Form test', () => {
     })
 
 
-     test.skip('should click multiple hobby ',async({page})=>{
+     test('should click multiple hobby ',async({page})=>{
      const Form = new form(page)    
         for(const combination of formVariable.hobbyCombinations){
             for(const hob of combination){
@@ -179,7 +184,7 @@ test.describe('Form test', () => {
      })
 
 
-        test.skip('should click multiple hobby checkboxes',async({page})=>{
+        test('should click multiple hobby checkboxes',async({page})=>{
         const Form = new form(page)
         const hobbiesList = formVariable.hobbyCombinations[6] //['Sports', 'Reading','Music']
 
@@ -194,7 +199,7 @@ test.describe('Form test', () => {
 
 
 
-    test.skip('should successfully upload multiple files',async({page})=>{
+    test('should successfully upload multiple files',async({page})=>{
         const Form = new form(page)
        await Form.addImage('image/camera.png')
        await Form.addImage('image/canon.pdf')
@@ -235,7 +240,7 @@ test.describe('Form test', () => {
 
 
 
-     test.skip('should click each state',async({page})=>{
+     test('should click each state',async({page})=>{
         const Form = new form(page)
         const states = Object.keys(stateCityMapping);
         console.log(states.length)
